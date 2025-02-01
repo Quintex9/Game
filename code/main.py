449c68188709581d563e730f73c240e2de0146b6
jeerikje <159952979+jeerikje@users.noreply.github.com>
2025-02-01 11:13:42 +0100

    pridanie kolízicie hráča s vlajkou ako spôsob ukončenia hry, po náraze s autom máte možnosť reštartovať hru, odstránenie 3 stromov, ktoré zavádzali na ceste

--- code/main.py
+++ code/main.py
@@ -5,98 +5,122 @@ from player import Player
 from random import choice
 from sprite import Sprite, Sprite2
 
-class Sprites(pygame.sprite.Group):
-	def __init__(self):
-		super().__init__()
-		self.offset = pygame.math.Vector2()
-		self.background = pygame.image.load('../graphics/main/map.png')
-
-	def vykresli(self):
-		self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
-		self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
-
-		display.blit(self.background, -self.offset)
-
-		for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-			offset_position = sprite.rect.topleft - self.offset
-			display.blit(sprite.image, offset_position)
-
 pygame.font.init()
+
+class Sprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.background = pygame.image.load('../graphics/main/map.png')
+
+    def vykresli(self):
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        display.blit(self.background, -self.offset)
+
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_position = sprite.rect.topleft - self.offset
+            display.blit(sprite.image, offset_position)
+
+# Koniec hry
+loading_screen = pygame.image.load('../graphics/main/over.jpg')
+restart_font = pygame.font.Font('freesansbold.ttf', 40)
+restart_text = restart_font.render("Stlač [R] pre reštart", True, "White")
+restart_text_rect = restart_text.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 100))
+
+# Font
 font = pygame.font.Font('freesansbold.ttf', 50)
 text_surface = font.render("Vyhral si! ", True, "White")
 text_rect = text_surface.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
-loading_screen = pygame.image.load('../graphics/main/over.jpg')
-restart_font = pygame.font.Font('freesansbold.ttf', 40)
-restart_text = restart_font.render("Stlač [R] pre reštart", True, "White")
-restart_text_rect = restart_text.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + -80))
-
+# Zoznam všetkých spritov
 sprites = Sprites()
 obstacles = pygame.sprite.Group()
 
+# Setup
 pygame.init()
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Hihi")
 clock = pygame.time.Clock()
 
-player = Player((2062, 6000), sprites, obstacles)
+player = Player((2062, 2200), sprites, obstacles)
 
-hudba = pygame.mixer.Sound('../audio/music.mp3')
-hudba.play(loops=-1)
+# Hudba
+#hudba = pygame.mixer.Sound('../audio/music.mp3')
+#hudba.set_volume(0.5)
+#hudba.play(loops=-1)
 
+# Timer
 timer = pygame.event.custom_type()
-pygame.time.set_timer(timer, 100)
+pygame.time.set_timer(timer, 20)
 
 pozicie = []
 
+# Pridanie objektov
 for file, position in SIMPLE_OBJECTS.items():
-	cesta = f'../graphics/objects/simple/{file}.png'
-	surface = pygame.image.load(cesta)
-	for pos in position:
-		Sprite(surface, pos, [sprites, obstacles])
+    cesta = f'../graphics/objects/simple/{file}.png'
+    surface = pygame.image.load(cesta)
+    for pos in position:
+        Sprite(surface, pos, [sprites, obstacles])
 
 for file, position in LONG_OBJECTS.items():
-	cesta = f'../graphics/objects/long/{file}.png'
-	surface = pygame.image.load(cesta)
-	for pos in position:
-		Sprite2(surface, pos, [sprites, obstacles])
+    cesta = f'../graphics/objects/long/{file}.png'
+    surface = pygame.image.load(cesta)
+    for pos in position:
+        Sprite2(surface, pos, [sprites, obstacles])
+
+# Pridanie koncového objektu
+end_image = pygame.image.load('../graphics/main/vlajka.png')
+end_image = pygame.transform.scale(end_image, (150, 150))
+end_sprite = pygame.sprite.Sprite()
+end_sprite.image = end_image
+end_sprite.rect = end_image.get_rect(topleft=(1550, 1050))
+end_sprite.rect.width /= 2 ; end_sprite.rect.height /= 2
+sprites.add(end_sprite)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == timer:
-			pozicia_auta = choice(CAR_START_POSITIONS)
-			if pozicia_auta not in pozicie:
-				pozicie.append(pozicia_auta)
-				Car(pozicia_auta, [sprites, obstacles])
-			if len(pozicie) > 5:
-				del pozicie[0]
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_r and player.game_over:
-				player.game_over = False
-				player.position = pygame.math.Vector2(2062, 6000)
-				player.direction = pygame.math.Vector2()
-				hudba.play(loops=-1)
-				pozicie.clear()
-				for sprite in sprites.sprites():
-					if hasattr(sprite, 'meno') and sprite.meno == 'auto':
-						sprite.kill()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	dt = clock.tick() / 1000
+        if event.type == timer:
+            pozicia_auta = choice(CAR_START_POSITIONS)
+            if pozicia_auta not in pozicie:
+                pozicie.append(pozicia_auta)
+                Car(pozicia_auta, [sprites, obstacles])
+            if len(pozicie) > 15:
+                del pozicie[0]
 
-	if not player.game_over:
-		display.fill((0, 0, 0))
-		if player.position.y >= 1000:
-			sprites.update(dt)
-			sprites.vykresli()
-		else:
-			hudba.stop()
-			display.fill("teal")
-			display.blit(text_surface, text_rect)
-	else:
-		display.blit(loading_screen, (0, 0))
-		display.blit(restart_text, restart_text_rect)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r and player.game_over:
+                player.game_over = False
+                player.position = pygame.math.Vector2(2062, 6000)
+                player.direction = pygame.math.Vector2()
+                #hudba.play(loops=-1)
+                pozicie.clear()
+                for sprite in sprites.sprites():
+                    if hasattr(sprite, 'meno') and sprite.meno == 'auto':
+                        sprite.kill()
 
-	pygame.display.update()
+    # deltatime
+    dt = clock.tick() / 1000
+
+    if not player.game_over:
+        display.fill((0, 0, 0))
+        sprites.update(dt)
+        sprites.vykresli()
+
+        # Kontrola kolízie s end_sprite
+        if player.rect.colliderect(end_sprite.rect):
+            player.game_over = True
+        if player.position.y <= 900:
+            player.position.y = 900
+
+    else:
+        #hudba.stop()
+        display.blit(loading_screen, (0, 0))
+        display.blit(restart_text, restart_text_rect)
+
+    pygame.display.update()
--- code/player.py
+++ code/player.py
@@ -1,8 +1,8 @@
-import pygame, sys
+import pygame,sys
 from os import walk
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, position, groups, collisions):
+    def __init__(self,position,groups,collisions):
         super().__init__(groups)
         self.game_over = False
 
@@ -10,41 +10,43 @@ class Player(pygame.sprite.Sprite):
         self.index = 0
         self.poz = 'down'
         self.image = self.animations[self.poz][self.index]
-        self.rect = self.image.get_rect(center=position)
+        self.rect = self.image.get_rect(center = position)
 
         self.position = pygame.math.Vector2(self.rect.center)
         self.direction = pygame.math.Vector2()
         self.speed = 200
 
+        #collisions
         self.collisions = collisions
-        self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
+        self.hitbox = self.rect.inflate(0,-self.rect.height/2)
 
     def hranice(self):
         if self.rect.left < 640:
-            self.position.x = 640 + self.rect.width / 2
+            self.position.x = 640 + self.rect.width/2
             self.hitbox.left = 640
             self.rect.left = 640
         if self.rect.right > 2560:
-            self.position.x = 2560 - self.rect.width / 2
+            self.position.x = 2560 - self.rect.width/2
             self.hitbox.right = 2560
             self.rect.right = 2560
         if self.rect.bottom > 6100:
-            self.position.y = 6100 - self.rect.height / 2
+            self.position.y = 6100 - self.rect.height/2
             self.hitbox.centery = self.rect.centery
             self.rect.bottom = 6100
 
-    def collision(self, dir):
+    def collision(self,dir):
         if dir == 'horizontalne':
             for sprite in self.collisions.sprites():
                 if sprite.hitbox.colliderect(self.hitbox):
-                    if hasattr(sprite, 'meno') and sprite.meno == 'auto':
+                    #najprv či existuje atribut a potom meno
+                    if hasattr(sprite,'meno') and sprite.meno == 'auto':
                         self.game_over = True
 
-                    if self.direction.x > 0:
+                    if self.direction.x > 0:  #pohyb vpravo
                         self.hitbox.right = sprite.hitbox.left
-                        self.rect.centerx = self.hitbox.centerx
+                        self.rect.centerx = self.hitbox.centerx #pri pohybe hrača do long objektu to triaslo obraz tak tu je fix
                         self.position.x = self.hitbox.centerx
-                    if self.direction.x < 0:
+                    if self.direction.x < 0:  #pohyb vlavo
                         self.hitbox.left = sprite.hitbox.right
                         self.rect.centerx = self.hitbox.centerx
                         self.position.x = self.hitbox.centerx
@@ -52,7 +54,7 @@ class Player(pygame.sprite.Sprite):
             if dir == 'vertikalne':
                 for sprite in self.collisions.sprites():
                     if sprite.hitbox.colliderect(self.hitbox):
-                        if hasattr(sprite, 'meno') and sprite.meno == 'auto':
+                        if hasattr(sprite,'meno') and sprite.meno == 'auto':
                             self.game_over = True
 
                         if self.direction.y > 0:
@@ -66,30 +68,33 @@ class Player(pygame.sprite.Sprite):
 
     def obrazky(self):
         self.animations = {}
-        for index, folder in enumerate(walk('../graphics/player')):
+        for index,folder in enumerate(walk('../graphics/player')):
             if index == 0:
                 for name in folder[1]:
                     self.animations[name] = []
             else:
                 for file in folder[2]:
-                    cesta = folder[0].replace('\\', '/') + '/' + file
+                    cesta = folder[0].replace('\\','/') + '/' + file
                     surface = pygame.image.load(cesta)
                     key = folder[0].split('\\')[1]
                     self.animations[key].append(surface)
 
-    def move(self, dt):
+
+
+    def move(self,dt):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
         self.position.x += self.direction.x * self.speed * dt
-        self.hitbox.centerx = round(self.position.x)
-        self.rect.centerx = self.hitbox.centerx
+        self.hitbox.centerx = round(self.position.x) #toto pre kolízie
+        self.rect.centerx = self.hitbox.centerx # toto pre kreslenie
         self.collision('horizontalne')
 
         self.position.y += self.direction.y * self.speed * dt
         self.hitbox.centery = round(self.position.y)
         self.rect.centery = self.hitbox.centery
         self.collision('vertikalne')
+
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -111,7 +116,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.y = 0
 
-    def pohyb(self, dt):
+    def pohyb(self,dt):
         current = self.animations[self.poz]
 
         if self.direction.magnitude() != 0:
@@ -121,10 +126,11 @@ class Player(pygame.sprite.Sprite):
         else:
             self.index = 0
 
-        self.image = current[int(self.index)]
+        self.image = current[int(self.index)] #obrazky maju cele čísla nie floaty
 
-    def update(self, dt):
+    def update(self,dt):
         self.input()
         self.move(dt)
         self.pohyb(dt)
         self.hranice()
+
